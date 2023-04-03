490fc910ac6131171869fd4b9f7968c069b019e3
Marcelo Veras <marceloantonino.verass@gmail.com>
2023-04-03 09:00:29 -0300

    Created the database

---
+++ py/conexao.py
@@ -0,0 +1,9 @@
+import mysql.connector
+class DataBase:
+    def __init__(self):
+        self.connection=mysql.connector.connect (
+            host="localhost",
+            user="root",
+            password="",
+            database="barbearia"
+        )
